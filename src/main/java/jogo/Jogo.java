98c0715dcac358b0b08479b165e6390ef4d863b6
Ryan Serra da Silva <ryanserrasilva@id.uff.br>
2025-09-15 22:45:34 +0000

    teste com mockito

--- src/main/java/jogo/Jogo.java
+++ src/main/java/jogo/Jogo.java
@@ -2,37 +2,40 @@ package jogo;
 
 public class Jogo {
 
-	
 	/*
-	 * Se for o primeiro turno e a soma das faces dos dados cair 7 ou 11 você ganha o jogo;
-	 * Se for o primeiro turno e a soma das faces dos dados cair 2, 3 e 12 você perde o jogo; 
-	 * Se não cair nenhum desses valores, o valor é armazenado e passa para o segundo turno;
-	 * Se for o segundo turno e a soma das faces dos dados cair 7 novamente, você perde o jogo;
-	 * Se for o segundo turno, você continuando jogando os dados e só ganha se cair um numero igual ao anterior do primeiro turno;
+	 * Se for o primeiro turno e a soma das faces dos dados cair 7 ou 11 vocï¿½ ganha o jogo;
+	 * Se for o primeiro turno e a soma das faces dos dados cair 2, 3 e 12 vocï¿½ perde o jogo; 
+	 * Se nï¿½o cair nenhum desses valores, o valor ï¿½ armazenado e passa para o segundo turno;
+	 * Se for o segundo turno e a soma das faces dos dados cair 7 novamente, vocï¿½ perde o jogo;
+	 * Se for o segundo turno, vocï¿½ continuando jogando os dados e sï¿½ ganha se cair um numero igual ao anterior do primeiro turno;
 	 */
-	public boolean jogo(){
-		
-		Jogador jogador = new Jogador();
-		Dado dadinho1 = new Dado();
-		Dado dadinho2 = new Dado();
-		
-		int resultado = jogador.lancar(dadinho1, dadinho2);
-		
-		if(resultado == 7 || resultado == 11) {
-			return true;
-		}else if(resultado == 2 || resultado == 3 || resultado == 12){
-			return false;
-		}
-		
-		int ponto = resultado;
-		
-		resultado = jogador.lancar(dadinho1, dadinho2);
-	
-		while(resultado != ponto && resultado != 7){
-			resultado = jogador.lancar(dadinho1, dadinho2);
-		}
-		
-		return resultado == ponto;
-		
-	}
+    private Jogador jogador;
+    private Dado dado1;
+    private Dado dado2;
+
+    public Jogo(Jogador jogador, Dado dado1, Dado dado2) {
+        this.jogador = jogador;
+        this.dado1 = dado1;
+        this.dado2 = dado2;
+    }
+
+    public boolean jogo() {
+        int resultado = jogador.lancar(dado1, dado2);
+
+        if (resultado == 7 || resultado == 11) {
+            return true;
+        } else if (resultado == 2 || resultado == 3 || resultado == 12) {
+            return false;
+        }
+
+        int ponto = resultado;
+
+        resultado = jogador.lancar(dado1, dado2);
+
+        while (resultado != ponto && resultado != 7) {
+            resultado = jogador.lancar(dado1, dado2);
+        }
+
+        return resultado == ponto;
+    }
 }
--- src/test/java/jogo/JogoTest.java
+++ src/test/java/jogo/JogoTest.java
@@ -1,6 +1,56 @@
 package jogo;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+
+
+import java.beans.Transient;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class JogoTest {
-	
 
+        Jogador jogador = mock(Jogador.class);;
+
+        Dado dado = mock(Dado.class);
+
+        Jogo jogo = mock(Jogo.class);
+
+        @Test
+        public void teste(){
+            when(dado.numero()).thenReturn(2);
+            assertEquals(dado.numero(),2);
+
+        }
+
+        @Test
+        public void test2(){
+            Dado dado2 = mock(Dado.class);
+            when(dado.numero()).thenReturn(2);
+            when(dado2.numero()).thenReturn(3);
+            when(jogador.lancar(dado, dado2)).thenReturn(5);
+            assertEquals(5, jogador.lancar(dado, dado2));
+        }
+
+        @Test
+        public void jogadorVence(){
+            Dado dado2 = mock(Dado.class);
+            when(dado.numero()).thenReturn(3);
+            when(dado2.numero()).thenReturn(4);
+            when(jogador.lancar(dado, dado2)).thenReturn(7);
+            assertTrue(jogo.jogo());
+    
+        }
+
+        @Test
+        public void jogadorPerde(){
+            Dado dado2 = mock(Dado.class);
+            when(dado.numero()).thenReturn(1);
+            when(dado2.numero()).thenReturn(1);
+            when(jogador.lancar(dado, dado2)).thenReturn(2);
+            when(jogo.jogo()).thenReturn(false);
+            assertFalse(jogo.jogo());
+    
+        }
 }
